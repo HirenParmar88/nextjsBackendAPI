d09c75aaadcc75643b1843279d8f0ca7d2d175bd
HirenParmar88 <hirenparmar5105@gmail.com>
2024-10-25 17:35:51 +0530

    pg connection success

--- db.js
+++ db.js
@@ -1,14 +1,52 @@
 //database connections
-// db.js
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-// Create a pool (connection to the database)
-const pool = new Pool({
-  user: 'public',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'admin123',
-  port: 5432,  // Default PostgreSQL port
-});
+const client = new Client({
+  host:"localhost",
+  user:"postgres",
+  port:"5432",
+  password:"admin123",
+  database:"postgres"
+})
+client.connect();
 
-module.exports = pool;
+//Show data
+
+client.query(`select * from public.users`,(err, res)=>{
+  if(!err){
+    console.log(res.rows);
+  }else{
+    console.log("not connected db !!");
+  }
+})
+
+
+// INSERT data in pg    
+/*client.query(`insert into public.users("user_id","username","email","password") values (4,'ravi','ravi12@gmail.com','ravi12')`,(err,res)=>{
+  if(!err){
+    console.log(res.rows);
+  }else{
+    console.log("Record not inserted !!");
+  }
+})*/
+
+//UPDATE data
+
+/*client.query("UPDATE public.users SET username='aaa' WHERE user_id=3", (err, res) => {
+  if (!err) {
+    console.log("Update successful:", res.rows);  // Log row count for affected rows
+  } else {
+    console.log("Error:", err.message);  // Log error message for debugging
+  }
+})*/
+
+//delete data
+client.query('delete from users where user_id=4',(err,res)=>{
+  if(!err){
+    console.log("deleted successfully");
+  }else{
+    console.log("Error !!");
+  }
+})
+
+client.end;
